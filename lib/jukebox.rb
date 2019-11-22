9f042d73fed37d5d7a01fc1b9e275a4c8b1e0828
Scott Hoisington <hoyzington@gmail.com>
2019-11-22 08:10:37 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -12,7 +12,15 @@ def songs
 end
 
 
-songs = 
+songs = ["Phoenix - 1901",
+  "Tokyo Police Club - Wait Up",
+  "Sufjan Stevens - Too Much",
+  "The Naked and the Famous - Young Blood",
+  "(Far From) Home - Tiga",
+  "The Cults - Abducted",
+  "Phoenix - Consolation Prizes",
+  "Harry Chapin - Cats in the Cradle",
+  "Amos Lee - Keep It Loose, Keep It Tight"]
 
 
 def help
